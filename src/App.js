5e06bd18eebc07f59711cf16d2d2a49e1e975b7f
Hector Toro <alejotoro94@hotmail.com>
2022-06-06 20:16:43 -0500

    Se agrega el componente Layout para que nuestro contenido este centrando en un ancho de 1200px

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
-import Productos from './components/Productos'
+import Productos from './components/Productos';
+import Layout from './components/Layout';
 
 class App extends Component {
 
@@ -14,10 +15,12 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Productos 
-          agregarAlCarro={ () => console.log('Por el momento no hace nada....')}
-          productos={this.state.productos}
-        />
+        <Layout>
+          <Productos 
+            agregarAlCarro={ () => console.log('Por el momento no hace nada....')}
+            productos={this.state.productos}
+          />
+        </Layout>
       </div>
     )
   }
---
+++ src/components/Layout.js
@@ -0,0 +1,31 @@
+import { Component } from "react";
+
+const styles = {
+    layout: {
+        backgroundColor: '#fff',
+        color: '#0A283E',
+        alingItems: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    container: {
+        with: '1200px',
+    }
+}
+
+class Layout extends Component {
+
+    render() {
+        return (
+            <div style={styles.layout}>
+                <div style={styles.container}>
+                    {this.props.children}
+                </div>
+            </div>
+        )
+    }
+
+}
+
+
+export default Layout;
