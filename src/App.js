2968e6573bdce6d820928d585a7c705163a382c4
Hector Toro <alejotoro94@hotmail.com>
2022-06-07 19:20:09 -0500

    Agregando elementos al carro de compras cuando empieza a sumar más de un elemento del mismo producto

--- src/App.js
+++ src/App.js
@@ -19,6 +19,15 @@ class App extends Component {
 
   agregarAlCarro = (producto) => {
     //console.log(producto)
+    const { carro } = this.state;
+    if ( carro.find( x => x.name === producto.name)) {
+      const newCarro = carro.map( x => x.name === producto.name ? ({
+        ...x,
+        cantidad: x.cantidad + 1
+      })
+      : x)
+      return this.setState( {carro: newCarro});
+    }
     return this.setState({
       carro: this.state.carro.concat(
         {...producto, cantidad: 1,}
